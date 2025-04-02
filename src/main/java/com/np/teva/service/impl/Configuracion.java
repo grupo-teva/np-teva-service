f604c2aaa51404aca63a3cf7826f00711e65cad0
jariquelme77 <juanantonioriquelme@grupoteva.com>
2025-04-02 13:50:15 +0200

    MLG-126: VALIDACION - Proceso que limpia sanciones de permisos generados después del tránsito
    Insertar en t_auditoria, constantes de intranet, configuración en servicio.

---
+++ src/main/java/com/np/teva/service/impl/Configuracion.java
@@ -0,0 +1,44 @@
+package com.np.teva.service.impl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Configuracion {
+    @Value("${spring.datasource.url}")
+    private String validacionUrl;
+
+    @Value("${spring.datasource.username}")
+    private String validacionUser;
+
+    @Value("${spring.datasource.password}")
+    private String validacionPassword;
+
+    @Value("${spring.datasource.intranet.url}")
+    private String intranetUrl;
+
+    @Value("${spring.datasource.intranet.username}")
+    private String intranetUser;
+
+    @Value("${spring.datasource.intranet.password}")
+    private String intranetPassword;
+
+
+    public String getValidacionUrl() {return validacionUrl; }
+
+    public String getValidacionUser() {return validacionUser;}
+
+    public String getValidacionPassword() {return validacionPassword;}
+
+    public String getIntranetUrl() {
+        return intranetUrl;
+    }
+
+    public String getIntranetUser() {
+        return intranetUser;
+    }
+
+    public String getIntranetPassword() {
+        return intranetPassword;
+    }
+}
---
+++ src/main/java/com/np/teva/service/impl/DatabaseHandler.java
@@ -0,0 +1,30 @@
+package com.np.teva.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseHandler {
+
+    @Autowired
+    private Configuracion configuracion;
+
+    public DatabaseHandler(){
+    }
+
+    public Connection getConnectionValidacion() throws SQLException {
+        Connection con = null;
+        con = DriverManager.getConnection(configuracion.getValidacionUrl(), configuracion.getValidacionUser(), configuracion.getValidacionPassword());
+
+        return con;
+    }
+
+    public Connection getConnectionIntranet() throws SQLException {
+        Connection con = null;
+        con = DriverManager.getConnection(configuracion.getIntranetUrl(), configuracion.getIntranetUser(), configuracion.getIntranetPassword());
+
+        return con;
+    }
+}
